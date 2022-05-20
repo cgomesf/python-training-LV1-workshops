7ca8d4a03250b855acdb7bc97a501a4afc3a3c81
MicheleOllari <54710828+MicheleOllari@users.noreply.github.com>
2022-05-20 11:33:11 +0200

    cleaning

--- module-4/sources/report.py
+++ module-4/sources/report.py
@@ -2,6 +2,7 @@ class Page:
     def __init__(self, text):
         self.text = text
 
+
 class ValidExtensions:
     @staticmethod
     def is_pdf():
@@ -11,12 +12,22 @@ class ValidExtensions:
     def is_txt():
         return ".txt"
 
+
 class Report:
     NUMBER_OF_PAGES = 0
 
     @classmethod
     def number_of_pages(cls):
         return cls.NUMBER_OF_PAGES
+
+    @staticmethod
+    def with_valid_extension(name_with_extension: str):
+        is_valid = None
+        if ValidExtensions.is_pdf() in name_with_extension:
+            is_valid = "pdf"
+        elif ValidExtensions.is_txt() in name_with_extension:
+            is_valid = "txt"
+        return is_valid
 
     def __init__(self, filename: str):
         self.filename = filename
@@ -25,14 +36,6 @@ class Report:
     def add_page(self, page_text):
         self.pages.append(Page(page_text))
         Report.NUMBER_OF_PAGES += 1
-
-    def with_valid_extension(name_with_extension: str):
-        is_valid = None
-        if ValidExtensions.is_pdf() in name_with_extension:
-            is_valid = "pdf"
-        elif ValidExtensions.is_txt() in name_with_extension:
-            is_valid = "txt"
-        return is_valid
 
 
 def main():
