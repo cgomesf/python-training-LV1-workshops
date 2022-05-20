b9d5e8918e37b8e4a90e792b69775c7a3918a276
florian-diesnis <114467@sidel.com>
2022-05-20 15:25:29 +0200

    test

--- module-4/sources/report.py
+++ module-4/sources/report.py
@@ -2,6 +2,29 @@ class Page:
     def __init__(self, text):
         self.text = text
 
+class Report:
+    def __init__(self,filename):
+        self.filename = filename
+        self.page_num = 0
+        self.page_list = []
+
+    @classmethod
+    def with_valid_extension(self,filename):
+        correct_extension = ["pdf","txt"]
+        if filename.split(".")[-1] in correct_extension:
+            return self
+        else:
+            return None
+
+    def add_page(self,page):
+        self.page_list.append(page)
+        self.page_num = self.page_num + 1
+
+    def number_of_pages(self):
+        return self.page_num
+
+    def pages(self):
+        return self.page_list
 
 def main():
     page_1 = Page("First page")
@@ -19,7 +42,7 @@ def main():
     report.add_page(page_3)
 
     assert report.number_of_pages() == 3
-    for page in report.pages:
+    for page in report.pages():
       assert isinstance(page, Page)
 
     print("OK !")
