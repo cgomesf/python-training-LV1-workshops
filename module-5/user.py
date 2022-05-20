ed55bf6fdc7f1e1359a077bdf07066a8de325255
Rodolphe Gohard <rodolphe@gohard.fr>
2022-05-20 15:24:02 +0200

    practical comments on workshop 5

--- module-5/user.py
+++ module-5/user.py
@@ -2,17 +2,17 @@
 
 tim = AdminUser("tim")
 sheila = AdminUser("sheila")
-karen = CustomerUser("karen", 100)
+karen = CustomerUser("karen", 100) # first arg is name, second is balance
 julio = CustomerUser("julio", 25)
 vince = ModeratorUser("vince", {"flowers", "general"})
 kim = ModeratorUser("vince", {"general", "FAQ"})
 
-product1 = Product(40)
+product1 = Product(40) # first arg is price
 
 users = [tim, sheila, karen, julio, vince, kim]
 
 for user in users:
     print(str(user) + " - " + user.can_buy(product1))
 
-assert str(sheila) == "#2 sheila"
+assert str(sheila) == "#2 sheila" # format is #{id} {name}
 assert str(kim) == "#6 kim"
